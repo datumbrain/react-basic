f9c33b01d226e15323233f91d013f2bef4e68554
Salman Cheema <salman-cheema@hotmail.com>
2019-07-18 17:55:47 +0500

    userinput and output

---
+++ src/User Output/UserOutput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+const userOutput =() => {
+    return {
+        <div>
+          
+        </div>
+    }
+}
