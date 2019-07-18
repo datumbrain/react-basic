2968dbe5e8ac919d7e781d2ef9564d4347aba8fd
Salman Cheema <salman-cheema@hotmail.com>
2019-07-18 18:32:20 +0500

    user output.css and vulnerability fix

--- src/User Output/UserOutput.js
+++ src/User Output/UserOutput.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import React, { Component } from 'react';
 import './App.css';
+import './UserOutput.css';
 
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 const userOutput =(props) => {
     return {
-        <div>
+        <div className="UserOutput">
           <p> Lorem Ipsum </p>
           <p> Borum Dipsum </p>
         </div>
